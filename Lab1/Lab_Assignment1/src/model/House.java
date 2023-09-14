fb2fd0933d898938152413b18298b12a0a669deb
SandaAbhishekSagar <144849115+SandaAbhishekSagar@users.noreply.github.com>
2023-09-14 15:42:00 -0400

    Pushing all files of AED Assignment1

---
+++ Lab1/Lab_Assignment1/src/model/House.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author abhisheksagar
+ */
+public class House {
+    private String num;
+    private String street;
+    private String city;
+    private String pincode;
+
+    public String getNumber() {
+        return num;
+    }
+
+    public void setNumber(String num) {
+        this.num = num;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPincode() {
+        return pincode;
+    }
+
+    public void setPincode(String pincode) {
+        this.pincode = pincode;
+    }
+    
+}
